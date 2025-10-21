d16399fabd562738ad0cdad5747d614fe8675459
Mo <mo.deprez.mo@gmail.com>
2025-10-21 12:24:52 +0200

    oefening 02 gemaakt

---
+++ Oefening-02/js/script.js
@@ -0,0 +1,11 @@
+"use strict"
+
+let name = "Mo Deprez";
+let age = 21;
+let email = "mo.deprez@ap.be";
+let classGroup = "D3B";
+
+console.log(`Naam: ${name}`);
+console.log(`Leeftijd: ${age}`);
+console.log(`E-mailadres: ${email}`);
+console.log(`Klasgroep: ${classGroup}`);
